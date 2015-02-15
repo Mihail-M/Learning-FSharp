d8f8c15c562bf415ffc9aac290f94bcf1d5f5f05
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-16 01:51:09 +0300

    First homework

---
+++ task1_1/task1_1/Factorial.fs
@@ -0,0 +1,12 @@
+﻿
+let rec fact n = 
+    if n = 0 then 1
+    else n * fact (n - 1)
+
+[<EntryPoint>]
+let main argv = 
+
+    System.Console.WriteLine "Hey, enter any positive number, please!"
+    let n = System.Console.ReadLine()
+    printfn "%d" (fact (int n))
+    0 
---
+++ task1_2/task1_2/Fibonacci.fs
@@ -0,0 +1,13 @@
+﻿
+let rec fib n = 
+    if n <= 1 then n
+    else fib (n - 1) + fib (n - 2)
+
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.WriteLine "Enter any positive number!"
+    let n = System.Console.ReadLine();
+    for i in 1..(int n) do
+        printf "%d " (fib i)
+    0 
