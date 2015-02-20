2e99b9b06193775223cbffd3c6b6a477aeb8f703
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-20 20:49:03 +0300

    homework 2 task 1, 4

---
+++ Homework2/task1/task1/Program.fs
@@ -0,0 +1,9 @@
+﻿let reverse list = 
+    let rec iteration list result  =  
+        match list with
+          | [] -> result
+          | hd::tl ->  iteration  tl (hd :: result)
+    iteration list []
+
+printfn "%A \n" (reverse ["Dasha", "Sasha", "VLad", "Ivan", "Valera", "Grisha", "Kudren", "Ivan", "Ilya", "Artem", "Lida" ])
+
---
+++ Homework2/task4/task4/Program.fs
@@ -0,0 +1,13 @@
+﻿let firstEntry list element = 
+   let rec loop list element index = 
+       match list with 
+       | [] -> -1
+       | head :: tail -> if (element = head) then index
+                         else loop tail element (index + 1)
+
+   loop list element 0
+
+printfn "%d" (firstEntry [1..4] 5)
+printfn "%d" (firstEntry [1..3] 1)
+printfn "%d" (firstEntry [1..3] 3)
+
