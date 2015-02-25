42fbd91945de3cc9da69b04b44252e30a9f53801
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-25 03:04:27 +0300

    my integer list and function sum))

---
+++ IntegerList/IntegerList/Program.fs
@@ -0,0 +1,10 @@
+﻿type IntList = Nil | Cons of int*IntList
+
+let rec sumList list =
+    match list with
+        | Nil -> 0
+        | Cons(h, t) -> h + sumList t
+
+printf "%d" (sumList (Cons(2, Cons(3, Cons(-5,Nil)))))
+//may be I define it better
+//Later
