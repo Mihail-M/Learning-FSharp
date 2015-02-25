d15b1cb1a77ddb77b31f775a6598c54d73e7dd77
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-26 01:55:37 +0300

    Carring example

---
+++ Carring/Carring/Program.fs
@@ -0,0 +1,6 @@
+﻿let plus x y = x + y
+let plus2 = plus 2 
+
+printfn "%A" (List.map (fun x -> plus x 2 ) [1..10] )
+printfn "%A" (List.map (plus2) [1..10] )
+printfn "%A" (List.map (plus 2) [1..10] )
