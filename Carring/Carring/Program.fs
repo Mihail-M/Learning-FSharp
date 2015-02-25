2e6630b366ea997fb5abe48f394d565a2e2efa30
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-26 02:48:03 +0300

    existing element in list with carring

--- Carring/Carring/Program.fs
+++ Carring/Carring/Program.fs
@@ -4,3 +4,10 @@ let plus2 = plus 2
 printfn "%A" (List.map (fun x -> plus x 2 ) [1..10] )
 printfn "%A" (List.map (plus2) [1..10] )
 printfn "%A" (List.map (plus 2) [1..10] )
+
+//exist element on list
+let exist e = (List.fold ((||)) false) << (List.map ((=) e) )
+
+printfn "Existing element in list [1..10] 2 and 11: "
+printfn "2 - %b" (exist 2 [1..10])
+printfn "11 - %b" (exist 11 [1..10])
