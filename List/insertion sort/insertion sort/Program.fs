b8f1cb04ef8156e6b98277de72d890d49e6fb21d
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-25 03:20:48 +0300

    any restruct and add insertion sort of list

---
+++ List/insertion sort/insertion sort/Program.fs
@@ -0,0 +1,14 @@
+﻿let rec insertionSort l = 
+    let rec insert elem list = 
+        match list with
+            |[] -> [elem]
+            |h::t -> if elem > h then
+                        elem::list
+                     else h::(insert elem t)
+    
+    match l with
+        |[] -> []
+        |h::t -> insert h (insertionSort t)
+
+
+printf "%A" (insertionSort [1..10])
