d614055d61eb1890900fb2e27451c829d0cba3a6
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-27 21:37:58 +0300

    task3 homework3

---
+++ Homework3/task3/task3/Program.fs
@@ -0,0 +1,7 @@
+﻿let even1 l = l |> List.map (fun x -> x % 2) |> List.fold (fun acc x -> acc + x) 0
+let even2 l = l |> List.filter (fun x -> x % 2 = 0) |> List.length
+let even3 l = l |> List.fold (fun acc x -> (acc + 1) - x % 2 ) 0
+
+even1 [1..10] |> printfn "%A"
+even2 [1..10] |> printfn "%A"
+even3 [1..10] |> printfn "%A"
