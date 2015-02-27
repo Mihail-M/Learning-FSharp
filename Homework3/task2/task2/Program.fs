7e9b8b6fc933abc4524113043a837316785b4f8c
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-27 21:18:07 +0300

    fix typo

--- Homework3/task2/task2/Program.fs
+++ Homework3/task2/task2/Program.fs
@@ -1,10 +1,10 @@
-﻿let isUnic _list =
+﻿let isUniq _list =
    let  list = List.sort _list
    let forCompare = (List.zip (0::list) (list@[0]) ) |> List.map (fun (x,y) -> (x = y)) 
 
    not (List.fold (fun acc x -> acc || x) false forCompare)
 
-isUnic [1..10] |> printfn "%A"
-isUnic (1::[1..10]) |> printfn "%A"
+isUniq [1..10] |> printfn "%A"
+isUniq (1::[1..10]) |> printfn "%A"
 
     
