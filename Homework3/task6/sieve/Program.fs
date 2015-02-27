a23a6a5bc0d97c52768821aca6a5e0ce34725fb8
Mihail-M <Mihail-maryfich@yandex.ru>
2015-02-27 19:41:09 +0300

    task6 homework3

---
+++ Homework3/task6/sieve/Program.fs
@@ -0,0 +1,9 @@
+﻿let isPrime n = not ((Seq.exists (fun x -> n % x = 0)) (seq {2 .. n / 2}))
+
+let primes = Seq.filter isPrime (Seq.initInfinite (fun x -> x + 2))  
+printfn "%A" (primes)
+
+let f a b = 
+        match a b with
+        | (0 1) -> 3
+        | (_ _) -> 4
