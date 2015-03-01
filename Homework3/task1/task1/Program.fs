038f7f03c04f3247b2367c91034ac25196e70a60
Mihail-M <Mihail-maryfich@yandex.ru>
2015-03-01 20:51:03 +0300

    fix spaces

--- Homework3/task1/task1/Program.fs
+++ Homework3/task1/task1/Program.fs
@@ -1,6 +1,6 @@
 ﻿let listTepmlate =  [1; 5; 6; 2]
 let l = List.zip (0::listTepmlate) (listTepmlate @ [0]) 
-let listSumPair = List.map (fun (x, y) -> x+y) l
+let listSumPair = List.map (fun (x, y) -> x + y) l
 let listMax= List.max listSumPair
 
-printfn "%A"  (List.findIndex ((=)listMax ) listSumPair)
+printfn "%A"  (List.findIndex ( (=) listMax ) listSumPair)
